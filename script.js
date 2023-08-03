16e5bfb3fd2f30de511ecfc9f59377c10cad144d
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2023-08-03 15:41:52 +0700

    Managed to have the delete button working, but ..
    With the help of someone else. Anyways, I now know that whenever I dynamically create a new element in the DOM, and I want to target that specific element, then I should add the event listener in the same function in which I create the element. Doing it that way omits the check if element is in the DOM.

--- script.js
+++ script.js
@@ -1,30 +1,84 @@
 const input = document.querySelector('#task');
 const task = document.querySelector('#button_task');
 const group = document.querySelector('#button_group');
-const del = document.querySelector('#button_delete');
+
+function delButton() {
+    let tasks = document.querySelector('#task_items');
+    let tasksDel = document.querySelector('.new-task');
+    tasks.removeChild(tasksDel);
+}
+
+function delButtonGroup() {
+    let tasks = document.querySelector('#task_items');
+    let tasksDelGroup = document.querySelector('.new-task-group');
+    tasks.removeChild(tasksDelGroup);
+}
 
 task.addEventListener('click', () => {
     const tasks = document.querySelector('#task_items');
     const newTR = document.createElement('tr');
+    const newTD = document.createElement('td');
+    const newButton = document.createElement('button');
     newTR.classList.add('new-task');
-    newTR.innerHTML = '<td>' + input.value + '<button class="delete" id="button_delete">del</button>' + '</td>';
+    newButton.classList.add('delete');
+    newButton.id = 'button_delete';
+    newButton.textContent = "del";
+    newButton.addEventListener('click', delButton);
+    newTD.textContent = input.value;
     tasks.appendChild(newTR);
+    newTR.appendChild(newTD);
+    newTD.appendChild(newButton);
     input.value = "";
     input.focus();
 });
+
+// Shorter code using innerHTML instead of createElement, but I haven't figured out how to include the addEventListener for the new delete button
+// task.addEventListener('click', () => {
+//     const tasks = document.querySelector('#task_items');
+//     const newTR = document.createElement('tr');
+//     newTR.classList.add('new-task');
+//     newTR.innerHTML = '<td>' + input.value + '<button class="delete" id="button_delete">del</button>' + '</td>';
+//     tasks.appendChild(newTR);
+//     input.value = "";
+//     input.focus();
+// });
 
 group.addEventListener('click', () => {
     const tasks = document.querySelector('#task_items');
     const newTR = document.createElement('tr');
-    newTR.innerHTML = '<th>' + input.value + '<button class="delete" id="button_delete">del</button>' + '</th>';
+    const newTH = document.createElement('th');
+    const newButton = document.createElement('button');
+    newTR.classList.add('new-task-group');
+    newButton.classList.add('delete');
+    newButton.id = 'button_delete_group';
+    newButton.textContent = "del";
+    newButton.addEventListener('click', delButtonGroup);
+    newTH.textContent = input.value;
     tasks.appendChild(newTR);
+    newTR.appendChild(newTH);
+    newTH.appendChild(newButton);
     input.value = "";
     input.focus();
 });
 
-del.addEventListener('click', () => {
-    let tasks = document.querySelector('#task_items');
-    let tasksDel = document.querySelector('.new-task');
-    tasks.removeChild(tasksDel);
-    console.log('clicked');
-});
+// Shorter code using innerHTML instead of createElement, but I haven't figured out how to include the addEventListener for the new delete button
+// group.addEventListener('click', () => {
+//     const tasks = document.querySelector('#task_items');
+//     const newTR = document.createElement('tr');
+//     newTR.innerHTML = '<th>' + input.value + '<button class="delete" id="button_delete">del</button>' + '</th>';
+//     tasks.appendChild(newTR);
+//     input.value = "";
+//     input.focus();
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let del = document.querySelector('#button_delete');
+  
+    if (del) {
+      del.addEventListener('click', function () {
+        console.log('Button clicked!');
+      });
+    } else {
+      console.error("'#button_delete' not found.");
+    }
+  });
