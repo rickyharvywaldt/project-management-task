4c43bb9b125c58daa42a53bb81f9f7dfa0c5a93b
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2023-08-04 15:21:32 +0700

    Main feature done, but I still need to figure out
    How to click Enter to close the edit/input field.

--- main.js
+++ main.js
@@ -3,38 +3,81 @@ let task = document.getElementById('task');
 let msg = document.getElementById('msg');
 let taskItems = document.getElementById('task_items');
 let buttonTask = document.getElementById('button_task');
+let taskGroup = document.getElementById('task_group');
+let buttonGroup = document.getElementById('button_group');
 
-form.addEventListener('submit', (e) => {
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     formValidation();
+// });
+
+buttonTask.addEventListener('click', (e) => {
     e.preventDefault();
-    formValidation();
+    formValidationSingle();
 });
 
-let formValidation = () => {
+buttonGroup.addEventListener('click', (e) => {
+    e.preventDefault();
+    formValidationGroup();
+});
+
+let formValidationSingle = () => {
     if(task.value ===""){
         msg.innerHTML = "Task field is empty"
     } else {
-        acceptData();
+        acceptDataSingle();
+    }
+};
+
+let formValidationGroup = () => {
+    if(task.value ===""){
+        msg.innerHTML = "Task field is empty"
+    } else {
+        acceptDataGroup();
     }
 };
 
 let data = {};
 
-let acceptData = () => {
+let acceptDataSingle = () => {
     data['text'] = task.value;
-    createTask();
+    createTaskSingle();
 };
 
-let createTask = () => {
+let acceptDataGroup = () => {
+    data['text'] = task.value;
+    createTaskGroup();
+};
+
+let createTaskSingle = () => {
     taskItems.innerHTML += `
     <div>
-        <p>${data.text}</p>
+        <p contenteditable="true">${data.text}</p>
         <span class="options">
             <i onClick="editTask(this)" class="fas fa-edit"></i>
             <i onClick="deleteTask(this)" class="fas fa-trash-alt"></i>
+            <i class="fas fa-ellipsis-v"></i>
         </span>
     </div>
     `;
     task.value = "";
+    task.focus();
+};
+
+let createTaskGroup = () => {
+    taskGroup.innerHTML += `
+    <div>
+        <p contenteditable="true">${data.text}</p>
+        <span class="options">
+            <i onClick="editTask(this)" class="fas fa-edit"></i>
+            <i onClick="deleteTask(this)" class="fas fa-trash-alt"></i>
+            <i class="fas fa-ellipsis-v"></i>
+        </span>
+    </div>
+    `;
+    task.value = "";
+    task.focus();
 };
 
 let deleteTask = (e) => {
@@ -45,3 +88,5 @@ let editTask = (e) => {
     task.value = e.parentElement.previousElementSibling.innerHTML;
     e.parentElement.parentElement.remove();
 };
+
+
